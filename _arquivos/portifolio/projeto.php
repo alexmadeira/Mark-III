405085059927b5058792ba1b809398999850df3a
Alex Madeira <alex.c.madeira@gmail.com>
2015-07-24 15:40:37 -0300

    ajuste de logo da empresa

---
+++ _arquivos/portifolio/projeto.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1,user-scalable=no">
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="Content-Language" content="pt-br" />
+    <meta name="copyright" content="Copyright © 1990 - www.alexmadeira.com.br - Todos os Direitos Reservados" />
+    <meta name="email" content="alex.c.madeira@gmail.com" />
+    <meta name="author" content="Alex Madeira" />
+    <meta name="organization" content="Alex Madeira - Web Solution Developer & SEO analyst" />
+    <meta name="generator" content="Alex Madeira" />
+    <meta name="location" content="São Paulo, Brazil" />
+
+    <meta name="keywords" content="Web, Developer, Desenvolvedor, Programador, Analista, SEO, SEM, Criação, Site" />
+    <meta name="description" content="Desnvolvimento de sites e analise de SEO/SEM" />
+
+
+    <link rel="shortcut icon" href="" type="image/x-icon" />
+
+    <!--Facebook Tags-->
+    <meta property="og:type" content="article"/>
+    <meta property="og:title" content="Alex Madeira | Web Solution Developer & SEO analyst"/>
+    <meta property="og:url" content="http://www.alexmadeira.com.br"/>
+    <meta property="og:image" content="http://www.alexmadeira.com.br/public/img/alex-madeira-cartao-facebook.jpg"/>
+    <meta property="og:site_name" content="Alex Madeira"/>
+    <meta property="og:description" content="Desnvolvimento de sites e analise de SEO/SEM"/>
+    <!--End Facebook Tags-->
+    
+
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Alex Madeira | Web Solution Developer & SEO analyst</title>
+
+    <!-- Bootstrap -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,300' rel='stylesheet' type='text/css'>
+    <link href='<?=base_url('public/fonts/font-awesome-4.3.0/css/font-awesome.min.css')?>' rel='stylesheet' type='text/css'>
+
+    <link href="<?=base_url('public/css/bootstrap.min.css')?>" rel="stylesheet">
+    <link href="<?=base_url('public/fonts/font.css')?>" rel="stylesheet">
+    <link href="<?=base_url('public/css/loader.css')?>" rel="stylesheet">
+    <link href="<?=base_url('public/css/projeto.css')?>" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+		
+  </head>
+  <body>
+	<div class="loader">
+		<div class="loading"></div>
+	</div>
+
+	<div class="container-fluid main">
+    	<header class="row header" style="background-image: url('<?=base_url('/public/img/bg01.jpg')?>')">
+    		<a href="<?=site_url('home')?>" class="close transition" title="Encerrar o projeto"></a>
+			<div class="nav">
+				<ul>
+					<li>
+						<em>Categoria</em>
+						<p>Web</p>
+					</li>
+					<li>
+						<em>Projeto</em>
+						<p>E-Commerce</p>
+					</li>
+					<li>
+						<em>Agencia</em>
+						<p><a href="#" class="staMonica agencia">Sta Monica</a></p>
+					</li>
+					<li>
+						<em>Ano</em>
+						<p>2015</p>
+					</li>
+				</ul>
+			</div>
+			<a href="#" class="projetoLogo" style="background-image: url('<?=base_url('/public/upload/alex-madeira-game7-logo.png')?>')">Projeto Nome</a>
+			<div class="project">
+				<h1>Vidis litro abertis</h1>
+				<p>- Consetis adipiscings elitis. Pra lá -</p>
+			</div>
+			<img src="<?=base_url('/public/img/tranche_bas1.png')?>" alt="" title="" class="trancher1" />
+    	</header>
+    	<aside class="contentPage">
+	    	<div class="row apresentacao col-md-16">
+	    		<div class="container">
+	    			<div class="mobile">
+	    				<img src="<?=base_url('/public/upload/alex-madeira-game7-mobile-v1.png')?>" alt="" title="" class="iphone G L" width="200">
+	    				<img src="<?=base_url('/public/upload/alex-madeira-game7-mobile-v2.png')?>" alt="" title="" class="iphone M R" width="200">
+	    			</div>
+	    			<div class="produto">
+	    				<img src="<?=base_url('/public/upload/alex-madeira-game7-produto-fone.png')?>" alt="" title="" class="">
+	    			</div>
+	    		</div>
+	    	</div>
+			<div class="row separaca col-md-16" style="background-image: url('<?=base_url('/public/upload/alex-madeira-bg-game7-day-7.jpg')?>')">
+				<img src="<?=base_url('/public/img/tranche_bas2.png')?>" alt="" title="" class="trancher2" />
+				<div class="overlay"></div>
+				<img src="<?=base_url('/public/img/tranche_bas3.png')?>" alt="" title="" class="trancher3" />
+			</div>
+	    	<div class="row apresentacao col-md-16">
+	    		<div class="container">
+	    			<div class="descricao">
+		    			<h2>Vidis litro abertis</h2>
+		    			<hr>
+		    			<p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
+	    			</div>
+	    			<div class="desktop">
+	    				<img src="<?=base_url('/public/upload/alex-madeira-game7-desktop.png')?>" alt="" title="" class="iMac G L" width="600">
+	    			</div>
+		    	</div>
+	    	</div>
+			<div class="row viewMore col-md-16">
+				<div class="container">
+					<h2>Mussum ipsum cacilds, vidis litro abertis. </h2>
+					<p class="italic">Mussum ipsum cacilds, vidis litro abertis, vidis litro abertis. </p>
+				</div>
+			</div>
+			<div class="row projects">
+	    		<div class="col-md-8 project" data-sr='move 0px'>
+	    		  	<a href="<?=site_url('projeto')?>" class="transition all"></a>
+	    			<img src="<?=base_url('public/img/cover_cinemap.jpg')?>"  alt=""  title="" />
+	    			<h3>Mussum ipsum <span>cacilds, vidis</span> litro.</h3>
+	    			<a href="<?=site_url('projeto')?>" class="viewMore transition">Mussum ipsum cacilds <i class="fa fa-long-arrow-right fa-lg"></i></a>
+	    		</div>
+	    		<div class="col-md-8 project" data-sr='move 0px'>
+	    		  	<a href="<?=site_url('projeto')?>" class="transition all"></a>
+	    			<img src="<?=base_url('public/img/cover_cinemap.jpg')?>"  alt=""  title="" />
+	    			<h3>Mussum ipsum <span>cacilds, vidis</span> litro.</h3>
+	    			<a href="<?=site_url('projeto')?>" class="viewMore transition">Mussum ipsum cacilds <i class="fa fa-long-arrow-right fa-lg"></i></a>
+	    		</div>
+	    	</div>
+	    	<div class="row footer">
+	    		<div class="container">
+		    		<p  class="col-md-8 project">© 2014 - Portfólio Alex Madeira. All rights reserved</p>
+		    		<ul  class="col-md-8 project redes">
+		    			<li class="facebook"><a href="https://www.facebook.com/alex.c.madeira"><i class="fa fa-facebook"></i></a></li>
+		    			<li class="linkedin"><a href="https://br.linkedin.com/pub/alex-madeira/65/b84/604"><i class="fa fa-linkedin"></i></a></li>
+		    		</ul>
+		    	</div>
+	    	</div>
+	    </aside>
+	</div>
+     <!-- jQuery (necessary for Bootstrap's JavaScript plugins)-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+
+	<script type="text/javascript" src="<?=base_url('public/js/scrollReveal.js-master/scrollReveal.js')?>"></script>
+	<script type="text/javascript" src="<?=base_url('public/js/jquery.easing.1.3.js')?>"></script>
+
+	<script type="text/javascript">window.sr = new scrollReveal();</script>
+    <script src="<?=base_url('public/js/bootstrap.min.js')?>"></script>
+    <script>
+		var scene = document.getElementById('scene');
+
+		jQuery(function( $ ){
+		    $(".main").hide();
+		    $(".loader").show();
+		    $(window).load(function() {
+		        	$(".loader").fadeOut(function() {
+		            $(".main").fadeIn(1000);
+		      	  }); 
+		 	 });
+								
+		});
+
+	    $(document).ready(function(){
+	        
+	        $('nav ul li a').click(function(e) {
+					var anchor=$(this);
+					$('html, body').stop().animate({
+						scrollTop: $(anchor.attr('href')).offset().top
+					}, 1000, 'easeInOutExpo');
+					e.preventDefault();
+			});
+			
+			/*--Fade entre les pages--*/
+		
+			$("a.transition").click(function(event){
+				event.preventDefault();
+				linkLocation = this.href;
+				$(".main").fadeOut(1000, redirectPage);
+				
+				function redirectPage() {
+				window.location = linkLocation;
+				}    
+			});
+						
+		});
+	    
+	</script>
+  </body>
+</html>
